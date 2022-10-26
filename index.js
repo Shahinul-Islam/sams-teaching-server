8104f89e178ab87a7e8cfbfccda2153fa6841cc4
shahinul islam <shahinul37@outlook.com>
2022-10-27 00:51:17 +0600

    course details send

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 app.use(cors());
 
 const courses = require("./data/courses.json");
+const details = require("./data/details.json");
 
 app.get("/", (req, res) => {
   res.send("api is running ");
@@ -12,6 +13,11 @@ app.get("/", (req, res) => {
 
 app.get("/courses", (req, res) => {
   res.send(courses);
+});
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const detail = details.find((d) => d.id === id);
+  res.send(detail);
 });
 
 app.listen(port, () => {
